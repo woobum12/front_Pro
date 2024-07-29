d5e98cf5df928adc503eec42bc0facc3c510e643
woobum12 <woobum1216@gmail.com>
2024-07-29 14:45:17 +0900

    중간 정산

--- js/script1.js
+++ js/script1.js
@@ -18,3 +18,25 @@ function autoslide(){
         left:"0"
     },1000)
 }
+
+// tab-------------------------------
+$('.tabMenu li').click(function () { 
+    var idx = $(this).index()
+
+    $('.tabItem >*').hide().removeClass('on')
+    $('.tabItem >*').eq(idx).show().addClass('on')
+
+    $('.tabItem li').removeClass('on')
+    $(this).addClass('on')
+    
+});
+// menu
+$(function(){
+
+    $('header .nav >ul').hover(function () {
+        $(this).find('.submenu').stop().slideDown(1000)
+    },
+        function (){
+            $('.submenu').stop().slideUp(500)
+        });
+});
